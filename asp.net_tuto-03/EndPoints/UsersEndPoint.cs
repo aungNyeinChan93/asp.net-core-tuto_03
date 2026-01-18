7116f7bbae11f949e83623c4dfff48e65808ccee
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-01-18 21:40:57 +0630

    update endpoint

---
+++ asp.net_tuto-03/EndPoints/UsersEndPoint.cs
@@ -0,0 +1,59 @@
+﻿using asp.net_tuto_03.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_tuto_03.EndPoints
+{
+    public static class UsersEndPoint
+    {
+        public static void MapUserEndPoint(this WebApplication app)
+        {
+            // Users
+            app.MapGet("/users/{id:int?}", async (int? id) =>
+            {
+                if (id is not null)
+                {
+                    var user = UserRepo.GetUser(id);
+                    return Results.Ok(user);
+                }
+                return Results.Ok(UserRepo.GetUsers());
+            });
+
+            app.MapPost("/users", async ([FromBody] User? user) =>
+            {
+                if (user?.Id < 0)
+                {
+                    return Results.ValidationProblem(new Dictionary<string, string[]>()
+        {
+             //{"id",new[] {"User is not invalid" ,"user id is not invalid" } },
+             {"id",["user is not invalid", "user Id is not invalid"] },
+        });
+                }
+                UserRepo.AddUser(user);
+                return Results.Created($"/users/{user?.Id}", user);
+            }).WithParameterValidation();
+
+            app.MapPut("/users", async ([FromQuery] int? id, [FromBody] User? user) =>
+            {
+
+                if (id is not null && user is not null)
+                {
+                    var updatedUser = UserRepo.UpdateUser(id, user);
+                    return Results.Ok(updatedUser);
+                }
+
+                return Results.ValidationProblem(new Dictionary<string, string[]>()
+    {
+         { "message",["User update fail!"]}
+    });
+            }).WithParameterValidation();
+
+            app.MapDelete("/users/{id:int?}", async ([FromRoute] int? id) =>
+            {
+                if (id is null) return Results.Problem("Id is not found");
+
+                UserRepo.DeleteUser(id);
+                return Results.Ok(new { message = "Delete success!" });
+            });
+        }
+    }
+}
--- asp.net_tuto-03/Program.cs
+++ asp.net_tuto-03/Program.cs
@@ -1,3 +1,4 @@
+using asp.net_tuto_03.EndPoints;
 using asp.net_tuto_03.Models.Auth;
 using asp.net_tuto_03.Models.Employees;
 using asp.net_tuto_03.Models.Users;
@@ -59,53 +60,56 @@ app.MapDelete("/employees/{id:int?}", async ([FromRoute] int? id) =>
     return Results.Ok(new { message = "Delete success!" });
 });
 
-// Users
-app.MapGet("/users/{id:int?}", async (int? id) =>
-{
-    if(id is not null)
-    {
-        var user = UserRepo.GetUser(id);
-        return Results.Ok(user);
-    }
-    return Results.Ok(UserRepo.GetUsers());
-});
 
-app.MapPost("/users", async ([FromBody] User? user) =>
-{
-    if (user?.Id < 0)
-    {
-        return Results.ValidationProblem(new Dictionary<string, string[]>()
-        {
-             //{"id",new[] {"User is not invalid" ,"user id is not invalid" } },
-             {"id",["user is not invalid", "user Id is not invalid"] },
-        });
-    }
-    UserRepo.AddUser(user);
-    return Results.Created($"/users/{user?.Id}",user);
-}).WithParameterValidation();
+//// Users
+app.MapUserEndPoint();
 
-app.MapPut("/users", async ([FromQuery] int? id, [FromBody] User? user) =>
-{
+//app.MapGet("/users/{id:int?}", async (int? id) =>
+//{
+//    if(id is not null)
+//    {
+//        var user = UserRepo.GetUser(id);
+//        return Results.Ok(user);
+//    }
+//    return Results.Ok(UserRepo.GetUsers());
+//});
+
+//app.MapPost("/users", async ([FromBody] User? user) =>
+//{
+//    if (user?.Id < 0)
+//    {
+//        return Results.ValidationProblem(new Dictionary<string, string[]>()
+//        {
+//             //{"id",new[] {"User is not invalid" ,"user id is not invalid" } },
+//             {"id",["user is not invalid", "user Id is not invalid"] },
+//        });
+//    }
+//    UserRepo.AddUser(user);
+//    return Results.Created($"/users/{user?.Id}",user);
+//}).WithParameterValidation();
+
+//app.MapPut("/users", async ([FromQuery] int? id, [FromBody] User? user) =>
+//{
    
-    if(id is not null && user is not null)
-    {
-        var updatedUser = UserRepo.UpdateUser(id, user);
-        return Results.Ok(updatedUser);
-    }
+//    if(id is not null && user is not null)
+//    {
+//        var updatedUser = UserRepo.UpdateUser(id, user);
+//        return Results.Ok(updatedUser);
+//    }
 
-    return Results.ValidationProblem(new Dictionary<string, string[]>()
-    {
-         { "message",["User update fail!"]}
-    });
-}).WithParameterValidation();
+//    return Results.ValidationProblem(new Dictionary<string, string[]>()
+//    {
+//         { "message",["User update fail!"]}
+//    });
+//}).WithParameterValidation();
 
-app.MapDelete("/users/{id:int?}", async ([FromRoute] int? id) =>
-{
-    if(id is null) return Results.Problem("Id is not found");
+//app.MapDelete("/users/{id:int?}", async ([FromRoute] int? id) =>
+//{
+//    if(id is null) return Results.Problem("Id is not found");
 
-    UserRepo.DeleteUser(id);
-    return Results.Ok(new { message = "Delete success!" });
-});
+//    UserRepo.DeleteUser(id);
+//    return Results.Ok(new { message = "Delete success!" });
+//});
 
 
 // Auth
